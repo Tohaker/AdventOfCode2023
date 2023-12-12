7c15addf944e8bc2e4e3a1ce7d9ce9cc495de893
Miles Bardon <milesbardon@gmail.com>
2023-12-12 16:32:54 +0000

    Completing Part 1

--- src/day12/main.go
+++ src/day12/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
@@ -31,29 +33,81 @@ func ParseInput(input string) SpringsRecord {
 }
 
 func CreateRegexp(springs []int) regexp.Regexp {
-	regex := `^\\.*`
+	regex := `^\.*`
 
 	for i, v := range springs {
 		regex += fmt.Sprintf(`#{%d}`, v)
 
 		if i != len(springs)-1 {
-			regex += `\\.+`
+			regex += `\.+`
 		}
 	}
 
-	regex += `\\.*$`
+	regex += `\.*$`
 
 	return *regexp.MustCompile(regex)
 }
 
-func CreateValidPermutations(record SpringsRecord) []string {
-	re := CreateRegexp(record.springs)
+func ReplaceAtIndex(in string, r rune, i int) string {
+	out := []rune(in)
+	out[i] = r
+	return string(out)
+}
 
-	// Count number of ?
-	unknownCount := strings.Count(record.damaged, "?")
-	splitString := strings.Split(record.damaged, "")
+func CreatePermutations(s string, i int) []string {
+	permutations := make([]string, 0)
+
+	// If at the end of the string, return the string
+	if i == len(s) {
+		permutations = append(permutations, s)
+	} else {
+		// If s[i] == ?, recurse with . and # on different branches
+		if s[i] == '?' {
+			permutations = append(permutations, CreatePermutations(ReplaceAtIndex(s, '.', i), i+1)...)
+			permutations = append(permutations, CreatePermutations(ReplaceAtIndex(s, '#', i), i+1)...)
+		} else {
+			permutations = append(permutations, CreatePermutations(s, i+1)...)
+		}
+	}
+
+	// compile back into single list
+	return permutations
+}
+
+func FindValidPermutations(record SpringsRecord) []string {
+	re := CreateRegexp(record.springs)
 
 	validPermuations := make([]string, 0)
 
+	for _, perm := range CreatePermutations(record.damaged, 0) {
+		if re.MatchString(perm) {
+			validPermuations = append(validPermuations, perm)
+		}
+	}
+
 	return validPermuations
+}
+
+func Part1(input []string) int {
+	result := 0
+
+	for _, line := range input {
+		result += len(FindValidPermutations(ParseInput(line)))
+	}
+
+	return result
+}
+
+func main() {
+	absPath, _ := filepath.Abs("../../inputs/day12.txt")
+	content, err := os.ReadFile(absPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+
+	fmt.Printf("Part 1: %d\n", Part1(lines))
+	// fmt.Printf("Part 2: %d\n", Part2(lines))
 }
--- src/day12/main_test.go
+++ src/day12/main_test.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
-
-var testSolvedInput = []string{
-	"#.#.### 1,1,3",
-	".#...#....###. 1,1,3",
-	".#.###.#.###### 1,3,1,6",
-	"####.#...#... 4,1,1",
-	"#....######..#####. 1,6,5",
-	".###.##....# 3,2,1",
-}
 
 var testUnsolvedInput = []string{
 	"???.### 1,1,3",
@@ -21,20 +15,6 @@ var testUnsolvedInput = []string{
 	"????.#...#... 4,1,1",
 	"????.######..#####. 1,6,5",
 	"?###???????? 3,2,1",
-}
-
-func TestCreateRegex(t *testing.T) {
-	for _, tc := range testSolvedInput {
-		t.Run(fmt.Sprintf("Can create regex for %s", tc), func(t *testing.T) {
-			parsed := ParseInput(tc)
-
-			result := CreateRegexp(parsed.springs)
-
-			if result.Match([]byte(parsed.damaged)) {
-				t.Fatalf("%s does not match %s", result.String(), parsed.damaged)
-			}
-		})
-	}
 }
 
 func TestValidPermutations(t *testing.T) {
@@ -52,11 +32,32 @@ func TestValidPermutations(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("Find possible arrangements for %s", tc.input), func(t *testing.T) {
-			result := len(CreateValidPermutations(ParseInput(tc.input)))
+			result := len(FindValidPermutations(ParseInput(tc.input)))
 
 			if result != tc.expected {
 				t.Fatalf("%d does not match %d", result, tc.expected)
 			}
 		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	result := Part1(testUnsolvedInput)
+	expected := 21
+
+	if result != expected {
+		t.Fatalf("%d does not match %d", result, expected)
+	}
+}
+
+func TestPart1Real(t *testing.T) {
+	absPath, _ := filepath.Abs("../../inputs/day12.txt")
+	content, _ := os.ReadFile(absPath)
+
+	result := Part1(strings.Split(string(content), "\n"))
+	expected := 7753
+
+	if result != expected {
+		t.Fatalf("%d is not %d", result, expected)
 	}
 }
